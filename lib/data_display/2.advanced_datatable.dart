f790d2a25a26c2908b11f878e73f2ce73757729c
DESKTOP-IOVIGRD\mola <mercury1259@outlook.com>
2025-02-26 22:49:19 +0800

    20250226

--- lib/data_display/2.advanced_datatable.dart
+++ lib/data_display/2.advanced_datatable.dart
@@ -54,8 +54,15 @@ class _MyHomePageState extends State<MyHomePage> {
             }
           },
           columns: [
-            DataColumn(label: Text('Row no')),
-            DataColumn(label: Text('Value'))
+            DataColumn(label: Text('方法名称')),
+            DataColumn(label: Text('ReqNonceStr')),
+            DataColumn(label: Text('OpenUserID')),
+            DataColumn(label: Text('请求入参')),
+            DataColumn(label: Text('请求出参')),
+            DataColumn(label: Text('响应时间')),
+            DataColumn(label: Text('日志级别')),
+            DataColumn(label: Text('创建时间')),
+            DataColumn(label: Text('关联条数'))
           ],
           customTableFooter: (source, offset) {
             return _buildCustomFooter(source, offset, rowsPerPage);
@@ -66,13 +73,11 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Widget _buildCustomFooter(
-    AdvancedDataTableSource<dynamic> source, int offset, int rowsPerPage) {
+Widget _buildCustomFooter(AdvancedDataTableSource<dynamic> source, int offset, int rowsPerPage) {
   final maxPagesToShow = 6;
   final maxPagesBeforeCurrent = 3;
   final lastRequestDetails = source.lastDetails!;
-  final rowsForPager =
-      lastRequestDetails.filteredRows ?? lastRequestDetails.totalRows;
+  final rowsForPager = lastRequestDetails.filteredRows ?? lastRequestDetails.totalRows;
   final totalPages = (rowsForPager / rowsPerPage).ceil();
   final currentPage = (offset ~/ rowsPerPage) + 1;
   List<int> pageList = [];
@@ -119,10 +124,8 @@ Widget _buildCustomFooter(
                         }
                       : null,
                   style: ButtonStyle(
-                    minimumSize: WidgetStateProperty.all(
-                        Size(30, 30)), // ✅ 改为 WidgetStateProperty
-                    padding: WidgetStateProperty.all(
-                        EdgeInsets.zero), // ✅ 改为 WidgetStateProperty
+                    minimumSize: WidgetStateProperty.all(Size(30, 30)), // ✅ 改为 WidgetStateProperty
+                    padding: WidgetStateProperty.all(EdgeInsets.zero), // ✅ 改为 WidgetStateProperty
                   ),
                   child: Text(e.toString()),
                 ),
@@ -145,26 +148,85 @@ Widget _buildCustomFooter(
 }
 
 class RowData {
-  final int index;
-  final String value;
+  final String methodName;
+  final String reqNonceStr;
+  final String openUserID;
+  final String request;
+  final String response;
+  final String time;
+  final String logLevel;
+  final String createTime;
+  final String relatedCnt;
 
-  RowData(this.index, this.value);
+  RowData(this.methodName, this.reqNonceStr, this.openUserID, this.request, this.response, this.time, this.logLevel,
+      this.createTime, this.relatedCnt);
 }
 
 class ExampleSource extends AdvancedDataTableSource<RowData> {
-  final data = List<RowData>.generate(
-      335, (index) => RowData(index, 'Value for no. $index'));
+  final data = <RowData>[
+    RowData('a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1', 'i1'),
+    RowData('a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2', 'i2'),
+    RowData('a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3', 'i3'),
+    RowData('a4', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a5', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a6', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a7', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a8', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a9', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a10', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a11', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a12', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a13', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a14', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a15', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a16', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a17', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a18', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a19', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a20', 'b20', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a21', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a22', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a23', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a24', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a25', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a26', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a27', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a28', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a29', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+    RowData('a30', 'b30', 'c', 'd', 'e', 'f', 'g', 'h', 'i'),
+  ];
 
   @override
   DataRow? getRow(int index) {
     final currentRowData = lastDetails!.rows[index];
     return DataRow(cells: [
       DataCell(
-        Text(currentRowData.index.toString()),
+        Text(currentRowData.methodName),
       ),
       DataCell(
-        Text(currentRowData.value),
-      )
+        Text(currentRowData.reqNonceStr),
+      ),
+      DataCell(
+        Text(currentRowData.openUserID),
+      ),
+      DataCell(
+        Text(currentRowData.request),
+      ),
+      DataCell(
+        Text(currentRowData.response),
+      ),
+      DataCell(
+        Text(currentRowData.time),
+      ),
+      DataCell(
+        Text(currentRowData.logLevel),
+      ),
+      DataCell(
+        Text(currentRowData.time),
+      ),
+      DataCell(
+        Text(currentRowData.relatedCnt),
+      ),
     ]);
   }
 
@@ -172,8 +234,7 @@ class ExampleSource extends AdvancedDataTableSource<RowData> {
   int get selectedRowCount => 0;
 
   @override
-  Future<RemoteDataSourceDetails<RowData>> getNextPage(
-      NextPageRequest pageRequest) async {
+  Future<RemoteDataSourceDetails<RowData>> getNextPage(NextPageRequest pageRequest) async {
     return RemoteDataSourceDetails(
       data.length,
       data
